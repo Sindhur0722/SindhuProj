0669575010639bd0c998cf87e7641aa3ed53c72d
sindhu <sindhuradeveloper01@gmail.com>
2018-06-17 18:58:43 -0700

    initial commit

---
+++ src/main/java/com/sindhura/SindhuProj/SindhuProjApplication.java
@@ -0,0 +1,12 @@
+package com.sindhura.SindhuProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindhuProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SindhuProjApplication.class, args);
+	}
+}
